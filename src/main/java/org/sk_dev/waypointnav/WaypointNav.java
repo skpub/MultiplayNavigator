8f44e489aa9cbc05cf1aa51d50686e1b8342fa40
skpub <satodeyannsu@gmail.com>
2025-08-24 14:57:59 +0900

    first

---
+++ src/main/java/org/sk_dev/waypointnav/WaypointNav.java
@@ -0,0 +1,17 @@
+package org.sk_dev.waypointnav;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WaypointNav extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
